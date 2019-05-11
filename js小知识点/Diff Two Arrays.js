755f85fdb740acfcb960fcacf7e28d71b8e6ba08
hangfeng Wu <nicehangfeng@foxmail.com>
2019-05-11 21:51:12 +0800

    Add files via upload

---
+++ js小知识点/Diff Two Arrays.js
@@ -0,0 +1,17 @@
+// 比较两个数组，然后返回一个新数组，
+// 该数组的元素为两个给定数组中所有独有的数组元素。
+// 换言之，返回两个数组的差异。
+
+function diff(arr1, arr2) {
+  return arr1
+    .filter(function(x) {
+      return arr2.indexOf(x) == -1;
+    })
+    .concat(
+      arr2.filter(function(x) {
+        return arr1.indexOf(x) == -1;
+      })
+    );
+}
+
+diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
