a102e1a986653bf4441e9a938059919a3e155f27
hangfengnice <nicehangfeng@foxmail.com>
2019-09-02 22:38:54 +0800

    水平垂直居中 && quiz/css11

--- client/learnBooks/ECMAScript 6/16 Generator.js
+++ client/learnBooks/ECMAScript 6/16 Generator.js
@@ -1,138 +1,34 @@
-// Generator
 
-function* helloWorldGenerator() {
-  yield "hello";
-  yield "world";
-  return "ending";
+
+function* gen() {
+  yield 1;
+  return 2;
 }
 
-var hw = helloWorldGenerator();
-// 上面代码定义了一个 Generator 函数helloWorldGenerator，它内部有两个yield表达式（hello和world），即该函数有三个状态：hello，world 和 return 语句（结束执行）。
+let g = gen();
 
-// yield 表达式
+console.log(g.next().value, g.next().value);
 
-// 与 Iterator 接口的关系
-{
-  var myIterable = {};
-  myIterable[Symbol.iterator] = function*() {
-    yield 1;
-    yield 2;
-    yield 3;
-  };
-
-  console.log([...myIterable]);
-
-  // Generator 函数执行后，返回一个遍历器对象。该对象本身也具有Symbol.iterator属性，执行后返回自身。
-  function* gen() {
-    // some code
+async function f() {
+  try {
+    await Promise.reject("出错了");
+  } catch (e) {
+    console.log(e)
   }
-
-  var g = gen();
-  console.log(g[Symbol.iterator]() === g); // true
+  return await Promise.resolve("hello world");
 }
 
-// 2 next 方法的参数
-{
-  function* foo(x) {
-    var y = 2 * (yield x + 1);
-    var z = yield y / 3;
-    return x + y + z;
-  }
+// f().then(v => console.log(v));
 
-  var a = foo(5);
-  a.next(); // Object{value:6, done:false}
-  a.next(); // Object{value:NaN, done:false}
-  a.next(); // Object{value:NaN, done:true}
-
-  var b = foo(5);
-  b.next(); // { value:6, done:false }
-  b.next(12); // { value:8, done:false }
-  b.next(13); // { value:42, done:true }
+// console.log(Promise.resolve('1'))
+//如果传入的 value 本身就是 Promise 对象，则该对象作为 Promise.resolve 方法的返回值返回。  
+function fn(resolve){
+    setTimeout(function(){
+        resolve(123);
+    },3000);
 }
-{
-  // 再看一个通过next方法的参数，向 Generator 函数内部输入值的例子
-  function* dataConsumer() {
-    console.log("beforeStarted")
-    console.log("Started");
-    console.log(`1. ${yield}`);
-    console.log(`2. ${yield}`);
-    return "result";
-  }
+let p0 = new Promise(fn);
 
-  let genObj = dataConsumer();
-  genObj.next();
-  // Started
-
-  // genObj.next("a");
-  // // 1. a
-  // genObj.next("b");
-  // // 2. b
-}
-
-// 3 for...of 循环
-{
-  function* foo() {
-    yield 1;
-    yield 2;
-    yield 3;
-    yield 4;
-    yield 5;
-    return 6;
-  }
-
-  for (let v of foo()) {
-    console.log(v); // 1 2 3 4 5
-  }
-
-  // 上面代码使用for...of循环，依次显示 5 个yield表达式的值。这里需要注意，一旦next方法的返回对象的done属性为true，for...of循环就会中止，且不包含该返回对象，所以上面代码的return语句返回的6，不包括在for...of循环之中
-
-  // Generator 函数和for...of循环，实现斐波那契数列的例子
-  function* fibonacci() {
-    let [prev, curr] = [0, 1];
-    for (;;) {
-      yield curr;
-      [prev, curr] = [curr, prev + curr];
-    }
-  }
-
-  for (let n of fibonacci()) {
-    if (n > 1000) break;
-    console.log(n);
-  }
-
-  // 是对象可以使用 generator
-  function* objectEntries(obj) {
-    let propKeys = Reflect.ownKeys(obj);
-
-    for (let propKey of propKeys) {
-      yield [propKey, obj[propKey]];
-    }
-  }
-
-  let jane = { first: "Jane", last: "Doe" };
-
-  for (let [key, value] of objectEntries(jane)) {
-    console.log(`${key}: ${value}`);
-  }
-
-  // 方法2 
-  function* objectEntries1() {
-    let propKeys = Object.keys(this);
-
-    for (let propKey of propKeys) {
-      yield [propKey, this[propKey]];
-    }
-  }
-
-  let jane1 = { first: "Jane", last: "Doe" };
-
-  jane1[Symbol.iterator] = objectEntries1;
-
-  for (let [key, value] of jane1) {
-    console.log(`${key}: ${value}`);
-  }
-  // first: Jane
-  // last: Doe
-}
-
-
+let p1 = Promise.resolve(p0);
+// 返回为true，返回的 Promise 即是 入参的 Promise 对象。
+console.log(p0 === p1);
