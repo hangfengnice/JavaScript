5b2eda34fe9db1ecd13aa0356667367cd5512d03
hangfeng <nicehangfeng@foxmail.com>
2019-12-26 23:28:27 +0800

    数组对象相等

---
+++ quiz/03 js 数组对象相等 package/index.js
@@ -0,0 +1,33 @@
+let equalObject = (o1, o2) => {
+  if (Object.prototype.toString.call(o1) !== Object.prototype.toString.call(o2)) {
+    return false
+  }
+  if (!(o1 instanceof Object) || !(o2 instanceof Object)) {
+    return false;
+  }
+  if (Object.keys(o1).length !== Object.keys(o2).length) {
+    return false;
+  }
+  return Object.keys(o1).every(v => o1[v] === o2[v]);
+};
+
+let equalArray = equalObject;
+
+let equal = (o1, o2) => {
+  if (Object.prototype.toString.call(o1) !== Object.prototype.toString.call(o2)) {
+    return false
+  }
+  if (!(o1 instanceof Object) || !(o2 instanceof Object)) {
+    return false;
+  }
+  if (Object.keys(o1).length !== Object.keys(o2).length) {
+    return false;
+  }
+  return Object.keys(o1).every(v => {
+    if (o1[v] instanceof Object) {
+      return equal(o1[v], o2[v]);
+    } else {
+      return o1[v] === o2[v];
+    }
+  });
+};
