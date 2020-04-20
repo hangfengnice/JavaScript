fb2cd65d0cee464c69a9d26b3d5c65ceb16052c5
hangfeng <nicehangfeng@foxmail.com>
2020-04-20 23:21:48 +0800

    feat: es6 function

--- src/test.js
+++ src/test.js
@@ -1,17 +1,6 @@
-// var message = ['hell', 'hhh'].join('\n')
-// let message = 'numm \n' + 'hhhh'
-// console.log(message)
-
-let message =  `hllo\nhhh`
-console.log(message)
-
-console.log(String.raw`hello\n;;`)
-
-function raw(literals, ...substitutions) {
-  // console.log(object)
-  for (let i = 0; i < literals.length; i ++) {
-    console.log(literals.raw[i])
-  }
+function create() {
+  return () => arguments[0]
 }
+var test = create(5)
 
-raw`\n${1}hhh\\${2}2`
+console.log(test())
