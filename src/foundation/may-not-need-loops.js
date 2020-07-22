0c65b4f032a55d4231a0ccc57af8330fd642f011
hangfeng <nicehangfeng@foxmail.com>
2020-07-22 22:45:54 +0800

    function

--- src/foundation/may-not-need-loops.js
+++ src/foundation/may-not-need-loops.js
@@ -1,18 +1,51 @@
 const first = (xs) => xs[0];
 const rest = (xs) => xs.slice(1);
 
-const reduce = function (reduceFn, accumulator, iterable) {
-  for (let i of iterable) {
-    accumulator = reduceFn(accumulator, i)
-  }
-  return accumulator
+var reduce = function (f, acc, xs) {
+  if (xs.length == 0) return acc
+  return reduce(f, f(acc, first(xs)), rest(xs))
 }
 
-const all = xs => reduce((acc, x) => acc && x, true, xs)
+var sum = function (xs) {
+  return reduce((acc, x) => x + acc, 0, xs)
+}
 
-let ret = all([1, 2 ,3 ,4])
+var reverse = function (xs) {
+  return reduce((acc, x) => x.concat(acc), [], xs)
+}
 
-const para = (f, acc, xs) =>
-  xs.length === 0
-    ? acc
-    : para(f, f(acc, first(xs), xs), rest(xs));
+var map = function (f, xs) {
+  return reduce((acc, x) => acc.concat(f(x)), [], xs)
+}
+
+var filter = function (f, xs) {
+  return reduce((acc, x) => f(x) ? acc.concat(x) : acc, [], xs)
+}
+
+var para = function (f, acc, xs) {
+  if (xs.length == 0) return acc
+  return para(f, f(acc, first(xs), xs), rest(xs))
+}
+
+var unfold = function (f, seed) {
+  function go(f, seed, acc) {
+    var res = f(seed)
+    return res ? go(f, res[1], acc.concat([res[0]])) : acc
+  }
+  return go(f, seed, [])
+}
+
+let ret = unfold(x => {
+  if (x < 26) {
+    return [String.fromCharCode(x + 65), x + 1]
+  }
+}, 0)
+
+var range = function (i, count) {
+  return unfold(x => {
+    if (x <= count) {
+      return [x, x + 1]
+    }
+  }, i)
+}
+console.log(range(1, 5));
--- src/functional programming/@Brian Lonsdorf Hardcore Functional Programming in JavaScript.js
+++ src/functional programming/@Brian Lonsdorf Hardcore Functional Programming in JavaScript.js
@@ -1,84 +1,87 @@
-const moment = require('moment');
-const { prepend } = require('ramda');
-const always = curry((a, b) => a);
-const compose = (...fns) => (...args) =>
-  fns.reduceRight((res, fn) => [fn.call(null, ...res)], args)[0];
+const moment = require("moment");
+
+var memoize = (f) => {
+  const cache = {};
+  return (...args) => {
+    const argsStr = JSON.stringify(args);
+    return cache[argsStr] || (cache[argsStr] = f.apply(this, args));
+  };
+};
 
 function curry(fn) {
   const arity = fn.length;
+
   return function $curry(...args) {
     if (args.length < arity) {
       return $curry.bind(null, ...args);
     }
+
     return fn.call(null, ...args);
   };
 }
-const identity = (x) => x;
-var add = curry((a, b) => a + b);
-var chain = curry((fn, m) => m.chain(fn));
-var concat = curry((a, b) => a.concat(b));
-const eq = curry((a, b) => a === b);
-const filter = curry((fn, xs) => xs.filter(fn));
-var flip = curry((fn, a, b) => fn(b, a));
-var append = flip(concat);
+const match = curry((what, s) => s.match(what));
+const replace = curry((what, replacement, s) => s.replace(what, replacement));
+const filter = curry((f, xs) => xs.filter(f));
+const map = curry((f, xs) => xs.map(f));
+
+const compose = (...fns) => (...args) =>
+  fns.reduceRight((res, fn) => [fn.call(null, ...res)], args)[0];
+
+const toUpperCase = (x) => x.toUpperCase();
+const exclaim = (x) => `${x}!`;
+const shout = compose(exclaim, toUpperCase);
+const reduce = curry((fn, zero, xs) => xs.reduce(fn, zero));
+const head = (x) => x[0];
+const reverse = reduce((acc, x) => [x].concat(acc), []);
+const last = compose(head, reverse);
+const lastUpper = compose(toUpperCase, head, reverse);
+const loudLastUpper = compose(exclaim, toUpperCase, head, reverse);
+const trace = curry((tag, x) => {
+  console.log(tag, x);
+  return x;
+});
+const id = (x) => x;
+const prop = curry((prop, obj) => obj[prop]);
 
 class Container {
   constructor(x) {
     this.$value = x;
   }
+
   static of(x) {
     return new Container(x);
   }
   map(f) {
-    return new Container(f(this.$value));
+    return Container.of(f(this.$value));
   }
 }
-const prop = curry((prop, obj) => obj[prop]);
-
 class Maybe {
   static of(x) {
     return new Maybe(x);
   }
+
   get isNothing() {
     return this.$value == null;
   }
+
   constructor(x) {
     this.$value = x;
   }
+
   map(fn) {
     return this.isNothing ? this : Maybe.of(fn(this.$value));
   }
+
   inspect() {
     return this.isNothing ? "Nothing" : `Just(${inspect(this.$value)})`;
   }
 }
-const match = curry((reg, str) => str.match(reg));
 const safeHead = (xs) => Maybe.of(xs[0]);
-const map = curry((f, xs) => xs.map(f));
-const streetName = compose(map(prop("street")), safeHead, prop("addresses"));
-const withdraw = curry((amount, { balance }) =>
-  Maybe.of(balance >= amount ? { balance: balance - amount } : null)
-);
-const updateLedger = (account) => account;
-const remainingBalance = ({ balance }) => `Your balance is $${balance}`;
-const finishTransaction = compose(remainingBalance, updateLedger);
-// const getTwenty = compose(map(finishTransaction), withdraw(20));
-
-const maybe = curry((v, f, m) => {
-  if (m.isNothing) {
-    return v;
-  }
-  return f(m.$value);
-});
-
-const getTwenty = compose(
-  maybe("You're broke!", finishTransaction),
-  withdraw(20)
-);
 class Either {
   static of(x) {
     return new Right(x);
   }
+
   constructor(x) {
     this.$value = x;
   }
@@ -88,6 +91,7 @@ class Left extends Either {
   map(f) {
     return this;
   }
+
   inspect() {
     return `Left(${inspect(this.$value)})`;
   }
@@ -97,65 +101,113 @@ class Right extends Either {
   map(f) {
     return Either.of(f(this.$value));
   }
+
   inspect() {
     return `Right(${inspect(this.$value)})`;
   }
 }
+
 const left = (x) => new Left(x);
 
-const getAge = curry((now, user) => {
-  const birthDate = moment(user.birthDate, 'YYYY-MM-DD');
-  return birthDate.isValid()
-    ? Either.of(now.diff(birthDate, 'years'))
-    : left('Birth date could not be parsed');
-});
-// const trace = curry((tag, x) => {
-//   console.log(tag, x);
-//   return x
-// })
-const toString = String;
-const fortune = compose(append('If you survive, you will be '), toString, add(1));
-const zoltar = compose(map(console.log), map(fortune), getAge(moment()));
 
-let ret = zoltar({ birthDate: '2005-12-12' });
-// let ret1 = getAge(moment(), { birthDate: '2005-12-12' })
-
-console.log(ret);
 
 class Task {
   constructor(fork) {
     this.fork = fork;
   }
+
   static rejected(x) {
     return new Task((reject, _) => reject(x));
   }
+
   // ----- Pointed (Task a)
   static of(x) {
     return new Task((_, resolve) => resolve(x));
   }
+
   // ----- Functor (Task a)
   map(fn) {
-    return new Task((reject, resolve) => this.fork(reject, compose(resolve, fn)));
+    return new Task((reject, resolve) =>
+      this.fork(reject, compose(resolve, fn))
+    );
   }
+
   // ----- Applicative (Task a)
   ap(f) {
-    return this.chain(fn => f.map(fn));
+    return this.chain((fn) => f.map(fn));
   }
+
   // ----- Monad (Task a)
   chain(fn) {
-    return new Task((reject, resolve) => this.fork(reject, x => fn(x).fork(reject, resolve)));
+    return new Task((reject, resolve) =>
+      this.fork(reject, (x) => fn(x).fork(reject, resolve))
+    );
   }
+
   join() {
     return this.chain(identity);
   }
 }
-{
-  fork = (_, resolve) => resolve(3)
-}
-map(x => x + 2)
 
-{
-  fork = (reject, resolve) => this.fork(reject, compose(resolve, x => x + 3))
-}
-map(x => x * 2)
+const fs = require('fs')
+const path = require('path')
+// const jquery = require('jquery')
+// console.log(jquery);
+const split = curry((reg, str) => str.split(reg))
 
+// const readFile = filename => new Task((reject, result) => {
+//   fs.readFile(path.join(__dirname, filename), 'utf-8', (err, data) => (err ? reject(err) : result(data)));
+// });
+
+// const getJSON = curry((url, params) => new Task((reject, result) => {
+//   jquery.getJSON(url, params, result).fail(reject);
+// }));
+
+// // console.log($.getJSON);
+// console.log(getJSON('1', {}).fork(console.log, console.log));
+class Compose {
+  constructor(fgx) {
+    this.getCompose = fgx;
+  }
+
+  static of(fgx) {
+    return new Compose(fgx);
+  }
+
+  map(fn) {
+    return new Compose(map(map(fn), this.getCompose));
+  }
+}
+class IO {
+  constructor(fn) {
+    this.unsafePerformIO = fn;
+  }
+  // ----- Pointed IO
+  static of(x) {
+    return new IO(() => x);
+  }
+  // ----- Functor IO
+  map(fn) {
+    return new IO(compose(fn, this.unsafePerformIO));
+  }
+  // ----- Applicative IO
+  ap(f) {
+    return this.chain(fn => f.map(fn));
+  }
+  // ----- Monad IO
+  chain(fn) {
+    return this.map(fn).join();
+  }
+  join() {
+    return new IO(() => this.unsafePerformIO().unsafePerformIO());
+  }
+}
+const readFile = filename => new IO(() => fs.readFileSync(path.join(__dirname, filename), 'utf-8'));
+
+const print = x => new IO(() => {
+  console.log(x);
+  return x;
+});
+const cat = compose(map(print), readFile);
+let ret = cat('config.txt');
+console.log(typeof ret.unsafePerformIO());
