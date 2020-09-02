7a490f4a09db0041eef0feb8e8adc9fd76217460
hangfeng <nicehangfeng@foxmail.com>
2020-09-02 13:54:37 +0800

    feat: basic

--- src/frontendmaster/kyle Simpson/Deep JavaScript Foundations v3/main.js
+++ src/frontendmaster/kyle Simpson/Deep JavaScript Foundations v3/main.js
@@ -6,15 +6,55 @@ function formatTrend(trendRate) {
   return `${direction} ${Math.abs(trendRate)}`
 }
 
-var teacher = 'kyle'
-
-function otherClass () {
-  teacher = 'suzy'
-  topic = 'react'
-  console.log('welcome');
+class Workshop {
+  constructor(teacher) {
+    this.teacher = teacher
+  }
+  ask(question) {
+    console.log(this.teacher, question);
+  }
 }
 
-otherClass()
+class AnotherWorkshop extends Workshop {
+  speakup(msg) {
+    this.ask(msg)
+  }
+}
 
-console.log(teacher);
-console.log(topic);
+var method = (function defineMethod() {
+  var instance = new WeakMap()
+
+  return function method(obj, methodName, fn) {
+    Object.defineProperty(obj, methodName, {
+      get() {
+        console.log(this, 'inner');
+        if (!instance.has(this)) {
+          instance.set(this, {})
+        }
+        var methods = instance.get(this)
+        if (!(methodName in methods)) {
+          methods[methodName] = fn.bind(this)
+        }
+        return methods[methodName]
+      }
+    })
+  }
+})()
+
+function bindMethods(obj) {
+  debugger
+  for(let ownProp of Object.getOwnPropertyNames(obj)) {
+    if (typeof obj[ownProp] == 'function') {
+      method(obj, ownProp, obj[ownProp])
+    }
+  }
+}
+
+var JSRecentPart = new AnotherWorkshop('Kyle')
+
+bindMethods(Workshop.prototype)
+bindMethods(AnotherWorkshop.prototype)
+
+JSRecentPart.speakup('what is different part')
+
+setTimeout(JSRecentPart.speakup, 100, 'gret');
