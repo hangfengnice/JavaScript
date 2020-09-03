ee48feabf74882a0a8d46c7d33f0f29481017e4d
hangfeng <nicehangfeng@foxmail.com>
2020-09-03 14:02:21 +0800

    feat: functional

--- src/FrontendMaster/Bianca Gandolfo/JavaScript: From Fundamentals to Functional JS v2/main.js
+++ src/FrontendMaster/Bianca Gandolfo/JavaScript: From Fundamentals to Functional JS v2/main.js
@@ -17,4 +17,11 @@ let secondColor = suspects[1].color
 let [s1, s2] = [suspects[0].color, suspects[1].color]
 
 let [{color}, {color: color1}] = suspects
-console.log(color, color1);
+// console.log(color, color1);
+
+var arr = [1, , ,2, 3]
+
+let arr1 = arr.map(item => item + 1)
+console.log(arr1);
+
+
--- src/FrontendMaster/Kyle Simpson/Functional-Light JavaScript v3/main.js
+++ src/FrontendMaster/Kyle Simpson/Functional-Light JavaScript v3/main.js
@@ -9,5 +9,19 @@ function f(...args) {
 }
 
 var g = flip(f)
-let ret = g(1, 2, 3, 4)
+// let ret = g(1, 2, 3, 4) // [2, 1, 3, 4]
+// console.log(ret);
+
+var add = x => x + 1
+var double = x => x * 2
+var subtract = x => x - 2
+
+function compose(...fns) {
+  // code below
+  return function (x) {
+    return fns.reduceRight((acc, fn) => fn(acc), x)
+  }
+}
+var f = compose(add, double, subtract)
+let ret = f(3) // 3
 console.log(ret);
