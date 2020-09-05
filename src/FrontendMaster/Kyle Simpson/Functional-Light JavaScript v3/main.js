c86782b1b0243925d499bff803f68455e966ca5b
hangfeng <nicehangfeng@foxmail.com>
2020-09-05 23:48:57 +0800

    feat

--- src/FrontendMaster/Kyle Simpson/Functional-Light JavaScript v3/main.js
+++ src/FrontendMaster/Kyle Simpson/Functional-Light JavaScript v3/main.js
@@ -1,4 +1,5 @@
-const {curry, ramda} = require('ramda')
+const {curry, ramda, reduce} = require('ramda')
+// const {reduce, map} = require('lodash')
 function flip(f) {
   return function flipped(a, b, ...args) {
     return f(b, a, ...args)
@@ -42,7 +43,7 @@ function f() {
 }
 
 let res = f()
-console.log(res)
+// console.log(res)
 
 
 // function compose(...fns) {
@@ -50,3 +51,68 @@ console.log(res)
 //     return fns.reduceRight((acc, fn) => fn(acc), x)
 //   }
 // }
+
+function Just (val) {
+  return {map, chain, ap}
+  function map(fn) {
+    return Just(fn(val))
+  }
+  function chain(fn) {
+    return fn(val)
+  }
+  function ap(anotherMonad) {
+    return anotherMonad.map(val)
+  }
+}
+
+var fo = Just(41)
+var ft = fo.map(function (v) {
+  return v + 1
+})
+
+var identity = v => v
+
+let a1 = fo.chain(identity)
+let a2 = ft.chain(identity)
+
+console.log(a1, a2);
+
+var user1 = Just('Kyel')
+var user2 = Just('Susan')
+
+var tuple = curry(function (x, y) {
+  return [x, y]
+})
+var users = user1.map(tuple).ap(user2).chain(identity)
+console.log(users);
+// const reduce = function (list, cb, initial) {
+//   let memo = initial
+
+//   for(let i = 0; i < list.length; i ++) {
+//     if (i == 0 && memo === undefined) {
+//       memo = list[0]
+//     } else {
+//       memo = cb(list[i], memo)
+//     }
+//   }
+//   return memo
+// }
+// let result = reduce([1, 2,3], (a, b) => a + b, 1)
+// console.log(result);
+
+const findsomeone = () => {
+  const speak = () => console.log(who);
+  let who = 'hf1'
+  return speak
+}
+let someone = findsomeone()
+someone()
+
+
+function test() {
+  console.log('helo')
+}
+
+let funcobj = String(test)
+let other = !funcobj
+console.log(funcobj);
