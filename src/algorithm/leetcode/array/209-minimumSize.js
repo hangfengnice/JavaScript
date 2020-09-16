58c32f631f3a1f0bf04f276bcfdf234668f004e6
hangfeng <nicehangfeng@foxmail.com>
2020-09-16 23:28:49 +0800

    feat: algorithm

--- src/algorithm/leetcode/array/209-minimumSize.js
+++ src/algorithm/leetcode/array/209-minimumSize.js
@@ -1,21 +1,39 @@
-function minSize(s, nums) {
-  let l = 0,
-    r = -1,
-    sum = 0,
-    res = nums.length + 1;
-  while (l < nums.length) {
-    if (r + 1 < nums.length && sum < s) {
-      sum += nums[++r];
-    } else {
-      sum -= nums[l++];
-    }
-    if (sum >= s) {
-      res = Math.min(res, r - l + 1);
+var minSubArrayLen = function (s, nums) {
+  let n = nums.length;
+  if (n == 0) {
+    return 0;
+  }
+  let ans = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < n; i++) {
+    let sum = 0;
+    for (let j = i; j < n; j++) {
+      sum += nums[j];
+      if (sum >= s) {
+        ans = Math.min(j - i + 1, ans);
+        break;
+      }
     }
   }
-  if (res == nums.length + 1) return 0
-  return res;
-}
+  return ans;
+};
 
-let ret = minSize(7, [1, 2 ,2,2, 4, 3, 1 ,1])
+var minSubArrayLen1 = function (s, nums) {
+  let n = nums.length;
+  if (n == 0) return 0;
+  let ans = Number.MAX_SAFE_INTEGER;
+  let start = 0,
+    end = 0;
+  let sum = 0;
+  while (end < n) {
+    sum += nums[end];
+    while (sum >= s) {
+      ans = Math.min(ans, end - start + 1);
+      sum -= nums[start++];
+    }
+    end++;
+  }
+  return ans;
+};
+
+let ret = minSubArrayLen1(7, [1, 2, 2, 2, 4, 3, 1, 1]);
 console.log(ret);
