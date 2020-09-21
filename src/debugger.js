3b148ebfcdbcf42cc94091e9307b74c286d5e5a8
hangfeng <nicehangfeng@foxmail.com>
2020-09-21 22:52:56 +0800

    feat: algorigthm linkedlist

--- src/debugger.js
+++ src/debugger.js
@@ -14,3 +14,23 @@ var deleteNthNode = function (head, n) {
   slow.next = slow.next.next;
   return dummy.next;
 };
+
+// [1, 2, 3] 1
+var rotateRight = function (head, k) {
+  let fast = head;
+  let slow = head;
+
+  while (k--) {
+    if (fast && fast.next) fast = fast.next;
+    else fast = head;
+  }
+  if (fast == head) return head;
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  fast.next = head;
+  head = slow.next;
+  slow.next = null;
+  return head;
+};
