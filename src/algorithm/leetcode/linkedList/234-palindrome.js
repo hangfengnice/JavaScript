6fcc3c7cfab0c77b47740061d1450af9637ecf09
hangfeng <nicehangfeng@foxmail.com>
2020-09-22 07:49:56 +0800

    feat: algorithm stack

--- src/algorithm/leetcode/linkedList/234-palindrome.js
+++ src/algorithm/leetcode/linkedList/234-palindrome.js
@@ -14,3 +14,56 @@ function palindrome(head) {
   }
   return true;
 }
+
+// [1, 2, 1]
+var isPalindrome = function (head) {
+  let frontPointer = head;
+  return recursivelyCheck(head);
+
+  function recursivelyCheck(currentNode) {
+    if (currentNode) {
+      //1, 2, 1
+      if (!recursivelyCheck(currentNode.next)) return false;
+      if (currentNode.val != frontPointer.val) return false;
+      frontPointer = frontPointer.next;
+    }
+    return true;
+  }
+};
+
+var isPalindrome = function (head) {
+  if (!head) return true;
+  let firstHalfEnd = endOfFirstHalf(head);
+  let secondHalfStart = reverseList(firstHalfEnd.next);
+  let p1 = head;
+  let p2 = secondHalfStart;
+  let result = true;
+  while (result && p2) {
+    if (p1.val != p2.val) result = false;
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+
+  firstHalfEnd.next = reverseList(secondHalfStart);
+  return result;
+  function reverseList(head) {
+    let prev = null;
+    let cur = head;
+    while (cur) {
+      let nextTmp = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = nextTmp;
+    }
+    return prev;
+  }
+  function endOfFirstHalf(head) {
+    let fast = head;
+    let slow = head;
+    while (fast.next && fast.next.next) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
+};
