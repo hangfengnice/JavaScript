ddcc7414dae719444164e147377e8972193429b8
hangfeng <nicehangfeng@foxmail.com>
2020-09-29 08:52:50 +0800

    feat: frontendmaster

--- src/debugger.js
+++ src/debugger.js
@@ -1,2 +1,27 @@
+css hover 出现 删除
+https://github.com/zhangxinxu/quiz/issues/6
 
 
+dom 实现滑块 略难
+https://github.com/zhangxinxu/quiz/issues/19
+
+css 布局 加上一个 选中的状态
+https://github.com/zhangxinxu/quiz/issues/21
+
+
+css 布局 上下布局
+https://github.com/zhangxinxu/quiz/issues/28
+
+css 打点的实现
+https://github.com/zhangxinxu/quiz/issues/34
+
+
+css 下啦导航
+https://github.com/zhangxinxu/quiz/issues/37
+
+css 定位 图标的使用
+https://github.com/zhangxinxu/quiz/issues/44
+
+
+css 控件 3个 按钮
+https://github.com/zhangxinxu/quiz/issues/47
