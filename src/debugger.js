4e202dc6ecf6a99e12f4e036f530ae8ce04d14f1
hangfeng <nicehangfeng@foxmail.com>
2020-10-25 22:37:44 +0800

    feat

--- src/debugger.js
+++
@@ -1,8 +0,0 @@
-var numJewelsInStones = function (J, S) {
-  let sobj = {}, num = 0
-  S.split('').forEach(item => {
-    sobj[item] ? sobj[item] ++ : (sobj[item] = 1)
-  })
-  J.split('').forEach(item => sobj[item] ? (num += sobj[item]) : '')
-  return num
-}
