04cb6c385e084b18120a11e7b1b9e8f1c97892fb
hangfeng <nicehangfeng@foxmail.com>
2021-03-29 18:02:49 +0800

    fix

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
-function debounce(fn, wait) {
-  let timer
-  return function () {
-    clearTimeout(timer)
-
-    timer = setTimeout(() => {
-      fn.apply(this, arguments)
-    }, wait)
-  }
-}
-
-function throttle(fn, wait) {
-  let timer
-  return function () {
-    if (!timer) {
-      timer = setTimeout(() => {
-        fn.apply(this, arguments)
-        timer = null
-      }, wait)
+var findMaxConsecutiveOnes = function(nums) {
+  let i = 0
+  let j = 0
+  let len = 0
+  for(; j < nums.length; j ++) {
+    if (!nums[j]) {
+      len = Math.max(j - i, len)
+      i = j + 1
+    }else if (j == nums.length - 1) {
+      console.log(i, j);
+      len = Math.max(j - i, len)
     }
   }
-}
+  return len
+};
+
+let res = findMaxConsecutiveOnes([0 ,1,1, 0, 1, 1,1,0,1])
+console.log(res, 'res');
