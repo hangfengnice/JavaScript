70bd3c2944ad9cc1f54309d52a113d236c87f593
hangfeng <nicehangfeng@foxmail.com>
2021-04-01 09:59:01 +0800

    feat

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-var findMaxConsecutiveOnes = function(nums) {
-  let i = 0
-  let j = 0
-  let len = 0
-  for(; j < nums.length; j ++) {
-    if (!nums[j]) {
-      len = Math.max(j - i, len)
-      i = j + 1
-    }else if (j == nums.length - 1) {
-      console.log(i, j);
-      len = Math.max(j - i, len)
-    }
-  }
-  return len
-};
+function alias(name) {
+  return function aliasClosure() {
+      return this[name].apply(this, arguments);
+  };
+}
 
-let res = findMaxConsecutiveOnes([0 ,1,1, 0, 1, 1,1,0,1])
-console.log(res, 'res');
+
+let time = new Date()
+let year = time.getFullYear()
+let month = time.getMonth() + 1
+let date = time.getDate()
+
+let res = `${year}-${month < 10 ? '0' + month : month}-${date < 10 ? '0' + date : date}`
+
+console.log(res);
