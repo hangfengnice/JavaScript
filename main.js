6898e81f42207d7bd9955b4b9e8b177ab894fb1c
hangfeng <nicehangfeng@foxmail.com>
2021-09-06 22:01:55 +0800

    2

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-const repeat = (str = '', times = 1) => {
-  let result = ''
-  if (!str || times < 1) {
-    return result
+if(!Promise.prototype.finally) {
+  Promise.prototype.finally = function (fn) {
+    return this.then(
+      function (v) {
+
+      },
+      function (e) {
+        return Promise.resolve(fn()).then(function (v) {throw e})
+      }
+    )
   }
-  
-  debugger
-  do {
-    if (times % 2) {
-      result += str
-    }
-    times = Math.floor(times / 2)
-
-    if (times) {
-      str += str
-    }
-  } while(times)
-
-  return result
 }
-
-console.log(repeat('a', 9));
