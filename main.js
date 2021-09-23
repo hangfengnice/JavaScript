165887bf21ca9adf23fde05f2ffeb5be11fcc886
hangfeng <nicehangfeng@foxmail.com>
2021-09-23 11:25:11 +0800

    up

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-if(!Promise.prototype.finally) {
-  Promise.prototype.finally = function (fn) {
-    return this.then(
-      function (v) {
+let node = {
+    type: "Identifier",
+    name: "foo",
+  },
+  type = "Literal",
+  name = 5;
 
-      },
-      function (e) {
-        return Promise.resolve(fn()).then(function (v) {throw e})
-      }
-    )
-  }
+function outputInfo(value) {
+  console.log(value, "value");
+  console.log(value === node);
 }
+
+outputInfo(({ type, name } = node)); // true
+
+console.log(type); // "Identifier"
+console.log(name);
