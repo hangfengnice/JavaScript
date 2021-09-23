85ed4bf799ad42d8bcaefcd019aa266ad79a8b56
hangfeng <nicehangfeng@foxmail.com>
2021-09-23 20:33:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-let node = {
-    type: "Identifier",
-    name: "foo",
-  },
-  type = "Literal",
-  name = 5;
+let p1 = new Promise(function(resolve, reject) {
+  resolve(42);
+});
 
-function outputInfo(value) {
-  console.log(value, "value");
-  console.log(value === node);
-}
+let p2 = new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    reject(42);
+  }, 200);
+  
+});
 
-outputInfo(({ type, name } = node)); // true
+let p3 = new Promise(function(resolve, reject) {
+  reject(44);
+});
 
-console.log(type); // "Identifier"
-console.log(name);
+let p4 = Promise.all([p1, p2, p3]);
+
+p4.catch(function(value) {
+  console.log(Array.isArray(value))   // false
+  console.log(value);                 // 43
+});
