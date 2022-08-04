29a98c60a49c87606071fedb1f2923a34ff4c3d5
hangfeng <nicehangfeng@foxmail.com>
2022-08-04 15:08:18 +0800

    up

--- main.js
+++ main.js
@@ -1,94 +1,18 @@
-<template>
-  <div class="cost">
-    <div class="cost-inner">
-      <!-- <Caption caption="基础信息" />
-      <el-descriptions border class="margin-top" :column="2">
-        <el-descriptions-item :span="2">
-          <template slot="label">
-            <span>采购项目</span>
-          </template>
-        </el-descriptions-item>
-        <el-descriptions-item>
-          <template slot="label">
-            <span class="required">项目编号</span>
-          </template>
-          <div>PJ2098877</div>
-        </el-descriptions-item>
-        <el-descriptions-item>
-          <template slot="label">
-            <span>预算金额（元）</span>
-          </template>
-          <div>2899990999</div>
-        </el-descriptions-item>
-        <el-descriptions-item>
-          <template slot="label">
-            <span>所在城市</span>
-          </template>
-          <div>浙江省 杭州市</div>
-        </el-descriptions-item>
-        <el-descriptions-item>
-          <template slot="label">
-            <span class="required">所属行业</span>
-          </template>
-          <div>软件开发</div>
-        </el-descriptions-item>
-
-        <el-descriptions-item>
-          <template slot="label">
-            <span>所属岗位</span>
-          </template>
-          <el-select v-model="job" class="job-select" placeholder="请选择">
-            <el-option
-              v-for="item in jobs"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </el-descriptions-item>
-        <el-descriptions-item>
-          <template slot="label">
-            <span></span>
-          </template>
-          <el-select style="opacity: 0"></el-select>
-        </el-descriptions-item>
-      </el-descriptions> -->
-    </div>
-  </div>
-</template>
-
-<script>
-  const job = '项目经理、产品经理、后端工程师、前端工程师、UI设计师、测试工程师'
-  export default {
-    name: 'Cost',
-    data() {
-      return {
-        jobs: [
-          {
-            label: job,
-            value: job,
-          },
-        ],
-        job,
-      }
-    },
-  }
-</script>
-
-<style lang="scss" scoped>
-  .cost {
-    padding: 8px;
-    .cost-inner {
-      border-radius: 4px;
-      padding: 8px;
-      box-sizing: border-box;
-      background: #fff;
-    }
-  }
-  .job-select {
-    width: 100%;
-  }
-  ::v-deep .el-descriptions-item__label.is-bordered-label.is-bordered-label {
-    width: 120px;
-  }
-</style>
+allArrivalDate: 1577808000000
+amount: 265
+billId: "703a5245546743eea90ee5f314d50eab"
+companyCd: "2227"
+companyName: "浙江金华分公司"
+contractAgtType: "框架合同"
+contractName: "签订的四期工程网络供货合同"
+contractNo: "合同[2017]2084号"
+fullInvoiceFlag: "N"
+orderBillName: "三费自动监理费订单-B20302227400102 永康石柱机房-台门机房72芯光缆线路工程等监理费"
+orderBillNo: "P22272200108"
+orderId: 3569733
+orderType: "工程类"
+payProportion: "100"
+rowId: 2
+taxAmountTotal: "265.00"
+totalAmount: "265.00"
+totalClaimAmount: "0.00"
