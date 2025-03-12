6cd940215809c925980cb19ee03b64243996d6c0
hangfeng <nicehangfeng@foxmail.com>
2025-03-12 09:35:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,13 @@
-var count = 1
-var container = document.getElementById('container')
+console.log(1 + 2)
 
-function getUserAction(event) {
-  console.log(this, event)
-  container.innerHTML = count++
-}
+//  甲 乙 丙
 
-container.onmousemove = throttle(getUserAction, 1000, {})
+// 20 24 12
 
-function throttle(func, wait, options) {
-  var timeout, context, args, result
-  var previous = 0
+let a = 0.875 * 1400
 
-  if (!options) {
-    options = {}
-  }
+console.log(a, 'a')
 
-  var later = function () {
-    previous = +new Date()
-    timeout = null
-    func.apply(this, context)
-  }
+// 1400
 
-  function throttled() {
-    var now = +new Date()
-
-    var remaining = wait - (now - previous)
-
-    context = this
-    args = arguments
-
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout)
-        timeout = null
-      }
-
-      previous = now
-      func.apply(context, args)
-    } else if (!timeout) {
-      timeout = setTimeout(later, remaining)
-    }
-  }
-
-  return throttled
-}
+// 200
